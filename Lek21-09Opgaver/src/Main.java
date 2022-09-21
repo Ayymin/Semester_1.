31620f63009ebba69a99d9c23309b2e5438b5caa
Ayyminq <amin.dahir@hotmail.com>
2022-09-21 13:37:58 +0200

    ok

---
+++ Lek21-09Opgaver/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        //Opgave 1b
+        for(int i=1;i<=99;i+=2) {
+         System.out.println(i);
+         }
+
+        //Opgave 1c
+        //Følgende statement = Såfremt i er mindre eller ligemed 300
+        //Så tælles der op med 1
+        // i % 3 betyder at vi tjekker om et tal går op i 3 tabellen
+        //med 0 i rest. Derfor kontrolleres der.
+        for (int i = 0; i <= 300; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+       }
+
+      //Opgave 3
+
+
+
+
+
+
+        }
+    }
